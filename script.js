5e5574de500a53f2b42afd99776e7377de06fed8
migle <mikigalletas@gmail.com>
2026-04-09 23:26:29 +0200

    añadir tareas

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 $(document).ready(function () {
-  $("#btn-mostrar-inputs-tarea").click(() => {
+  $("#btn-mostrar-inputs-tarea").click(function(){
+    //acción esconder
     if ($("#menu-tareas-escondible").is(":visible")) {
       $("#menu-tareas-escondible").hide();
       $("#btn-agregar-tarea").hide();
       $("#botones-menu").css({ "margin-top": 0 });
-      $(this).text("Añadir tarea");
+      $("#btn-mostrar-inputs-tarea").text("Añadir tarea");
+
+      $("#input-titulo-tarea").val("");
+      $("#input-cuerpo-tarea").val("");
+
+    //acción enseñar
     } else {
       $("#menu-tareas-escondible").show();
       $("#btn-agregar-tarea").show();
       $("#botones-menu").css({ "margin-top": "5%" });
-      $(this).text("Cancelar");
+      $("#btn-mostrar-inputs-tarea").text("Cancelar");
     }
   });
 
-  $("#btn-agregar-tarea").click(() => {
-    const nuevoTitulo = $("#input-titulo-tarea").val();
-    const nuevoCuerpo = $("#input-cuerpo-tarea").val();
-    listaTareas.push(new Tarea(nuevoTitulo, nuevoCuerpo));
+  $("#btn-agregar-tarea").click(function(){
+    let tarea = $("<div>").addClass("tarea");
+    let titulo = $("<p>").addClass("tarea-titulo").text($("#input-titulo-tarea").val());
+    let cuerpo = $("<p>").addClass("tarea-cuerpo").text($("#input-cuerpo-tarea").val());
+
+    $(tarea).append(titulo);
+    $(tarea).append(cuerpo);
+    $(tarea).append($("<button>").addClass("btn-completar-tarea").text("Marcar como completada"));
+    $(tarea).append($("<button>").addClass("btn-eliminar-tarea").text("Eliminar tarea"));
+    
+    $("#lista-tareas").prepend(tarea);
+
+    $("#input-titulo-tarea").val("");
+    $("#input-cuerpo-tarea").val("");
   });
 
-  class Tarea {
-    tarea(titulo, cuerpo) {
-      this.titulo = titulo;
-      this.cuerpo = cuerpo;
-    }
-  }
+  $("#lista-tareas").on("mouseenter", ".tarea", function(){
+    $(this).children().show();
+  });
 
-  const listaTareas = [];
+  $("#lista-tareas").on("mouseleave", ".tarea", function(){
+    $(this).find(".tarea-cuerpo").hide();
+    $(this).find(".btn-completar-tarea").hide();
+    $(this).find(".btn-eliminar-tarea").hide();
+  });
+
+  $("#lista-tareas").on("click", ".btn-completar-tarea", function(){
+    $(this).siblings(".tarea-titulo").css("background-color", rgb(117, 226, 122));
+    $(this).siblings(".tarea-cuerpo").css("background-color", rgb(179, 255, 175));
+  });
 });
